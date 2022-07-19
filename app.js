a75ed236162cd96f9313bebb6ce1cdbf9a564946
Embat <grimajenna80@gmail.com>
2022-07-19 09:41:10 +0200

    Added hiding and visible scroll bar for small screens Added a background gradient Fixed some styling and html

--- app.js
+++ app.js
@@ -5,3 +5,31 @@ menu.addEventListener('click', () => {
 	menu.classList.toggle('is-active')
 	menuLinks.classList.toggle('active')
 })
+
+// Code to make the scroll bar be visible or invisible depending on if you scroll or not
+
+let navbar = document.querySelector(".navbar")
+
+let hadMenuOpen = false;
+
+window.onscroll = function() {
+	let currentScrollPos = window.scrollY
+
+	if (currentScrollPos < 100) {
+		if (hadMenuOpen) {
+			hadMenuOpen = false
+
+			menu.classList.toggle('is-active', true)
+			menuLinks.classList.toggle('active', true)
+		}
+		navbar.style.top = "0px"
+	} else {
+		if (!hadMenuOpen) {
+			hadMenuOpen = menu.classList.contains('is-active')
+		}
+
+		menu.classList.toggle('is-active', false)
+		menuLinks.classList.toggle('active', false)
+		navbar.style.top = "-100px"
+	}
+}
